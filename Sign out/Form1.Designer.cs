313c9a497942ef6e895d78bc4691d8395cad059c
andre <andre@DESKTOP-03K317D>
2016-08-28 14:43:53 -0600

    Add project files.

---
+++ Sign out/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows.Forms;
+
+namespace Sign_out
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sign_out_btn = new System.Windows.Forms.Button();
+            this.cancel_btn = new System.Windows.Forms.Button();
+            this.countdown_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sign_out_btn
+            // 
+            this.sign_out_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.sign_out_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sign_out_btn.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sign_out_btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.sign_out_btn.Location = new System.Drawing.Point(209, 167);
+            this.sign_out_btn.Name = "sign_out_btn";
+            this.sign_out_btn.Size = new System.Drawing.Size(97, 49);
+            this.sign_out_btn.TabIndex = 0;
+            this.sign_out_btn.Text = "Sign Out";
+            this.sign_out_btn.UseVisualStyleBackColor = true;
+            this.sign_out_btn.Click += new System.EventHandler(this.log_off_button_click);
+            // 
+            // cancel_btn
+            // 
+            this.cancel_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancel_btn.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancel_btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.cancel_btn.Location = new System.Drawing.Point(383, 167);
+            this.cancel_btn.Name = "cancel_btn";
+            this.cancel_btn.Size = new System.Drawing.Size(97, 49);
+            this.cancel_btn.TabIndex = 1;
+            this.cancel_btn.Text = "Cancel";
+            this.cancel_btn.UseVisualStyleBackColor = true;
+            this.cancel_btn.Click += new System.EventHandler(this.cancel_button_click);
+            // 
+            // countdown_label
+            // 
+            this.countdown_label.AutoSize = true;
+            this.countdown_label.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdown_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.countdown_label.Location = new System.Drawing.Point(181, 102);
+            this.countdown_label.Name = "countdown_label";
+            this.countdown_label.Size = new System.Drawing.Size(377, 24);
+            this.countdown_label.TabIndex = 2;
+            this.countdown_label.Text = "Signing you out of Windows in 10 seconds...";
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Alert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(700, 250);
+            this.Controls.Add(this.countdown_label);
+            this.Controls.Add(this.cancel_btn);
+            this.Controls.Add(this.sign_out_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "CAEDM Logout";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sign_out_btn;
+        private System.Windows.Forms.Button cancel_btn;
+        private Label countdown_label;
+    }
+}
+
---
+++ Sign out/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+
+namespace Sign_out
+{
+    public partial class Form1 : Form
+    {
+        int time_left = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        public static void WindowsLogOff()
+        {
+            ExitWindowsEx(0, 0);
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void log_off_button_click(object sender, EventArgs e)
+        {
+            WindowsLogOff();
+        }
+
+        private void cancel_button_click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //launches the window fullscreen
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+
+            countdown_label.Text = ("Logging out in " + time_left.ToString());
+            //Starts timer
+            var timer = new Timer { Interval = 1000 };
+            timer.Tick += (o, args) =>
+            {
+                if(time_left == 0)
+                {
+                    WindowsLogOff();
+                    System.Windows.Forms.Application.Exit();
+                }
+                time_left--;
+                countdown_label.Text = ("Logging out in " + time_left.ToString());
+            };
+            timer.Start();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
